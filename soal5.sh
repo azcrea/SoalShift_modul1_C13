4c4d994578f674fb60da96ef2ca6cdef595af989
Mohammad Rizaldi Huzein Prastomo <36852188+tehelbaru@users.noreply.github.com>
2019-02-22 13:05:49 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,5 +1,3 @@
-loc=$(pwd)
-#mkdir /home/duhbuntu/modul1
-awk '/cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF < 13' >> /home/duhbuntu/modul1/nomor5.log
-
-2-30/6 * * * * /bin/bash /home/duhbuntu/sisop/prak1/soal5.sh
+loc=/home/duhbuntu
+if [[ ! -d "$loc/modul1"  ]]; then mkdir $loc/modul1; fi
+awk '/[Cc][Rr][Oo][Nn]/,!/[Ss][Uu][Dd][Oo]/' /var/log/syslog | awk 'NF < 13' >> $loc/modul1/nomor5.log

95440078142d1654b3423e6956e8d42d5be9981c
Mohammad Rizaldi Huzein Prastomo <36852188+tehelbaru@users.noreply.github.com>
2019-02-22 13:03:56 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -1,4 +1,5 @@
 date=$(date "+%H")
+loc=/home/duhbuntu/sisop/prak1
 file=$(date "+%H:%M %d-%B-%Y")
 
 low=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
@@ -6,4 +7,4 @@ hig=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 rot=$date
 
-cat /var/log/syslog | tr "${low:0:26}" "${low:rot:26}" | tr "${hig:0:26}" "${hig:rot:26}"  > "$file"
+cat /var/log/syslog | tr "${low:0:26}" "${low:rot:26}" | tr "${hig:0:26}" "${hig:rot:26}"  > "$loc/$file"

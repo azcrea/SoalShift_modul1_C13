c189b1f2770d1013bdfe50248b19aca1a0b78c7a
Mohammad Rizaldi Huzein Prastomo <36852188+tehelbaru@users.noreply.github.com>
2019-02-22 16:51:52 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 loc=/home/duhbuntu
 if [[ ! -d "$loc/modul1"  ]]; then mkdir $loc/modul1; fi
 awk '/[Cc][Rr][Oo][Nn]/,!/[Ss][Uu][Dd][Oo]/' /var/log/syslog | awk 'NF < 13' >> $loc/modul1/nomor5.log
